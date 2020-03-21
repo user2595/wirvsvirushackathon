dd76c5ae15a13b65bb231762a33efbcafc685f3a
Aurel Weinhold <aurel@weinhold.org>
2020-03-21 14:45:32 +0100

    simple first page to get used to controllers and views

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/LuckyController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class LuckyController extends AbstractController{
+
+    public function number() {
+        $number = random_int(0,100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+
+    }
+}
